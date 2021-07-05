86543a6771d9b2f7ad1f7a61fc36f70d62b51577
hyeji <khji1210@naver.com>
2021-07-06 00:33:43 +0900

    refactor: price 필드 추가

--- alarm/src/main/java/convenientstore/StockModified.java
+++ alarm/src/main/java/convenientstore/StockModified.java
@@ -4,12 +4,14 @@ public class StockModified extends AbstractEvent {
 
     private final Long id;
     private final int quantity;
+    private final int price;
     private final String status;
 
-    public StockModified(final Long id, final int quantity, final String status) {
+    public StockModified(final Long id, final int quantity, final int price, final String status) {
         super();
         this.id = id;
         this.quantity = quantity;
+        this.price = price;
         this.status = status;
     }
 
@@ -19,6 +21,10 @@ public class StockModified extends AbstractEvent {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public int getPrice() {
+        return price;
     }
 
     public String getStatus() {
